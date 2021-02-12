698a3db4decc2bfba5cf2ea7c105a5e7fdbabea6
abhinav <abhinav@staragrifinance.com>
2021-02-12 14:36:42 +0530

    first Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Catalog App")),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to 30 days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
